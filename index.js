12165313b2ba2dd857645e428ba64be953fb6eff
upalr <shovon.0323@gmail.com>
2019-10-21 17:01:20 +1100

    api authetication included

--- index.js
+++ index.js
@@ -91,9 +91,9 @@ app.get('/', (req, res) => {
 //  });
 //});
 
-app.get('/api/getTeacherDetails', verifyToken, (req, res) => {  
+app.get('/api/getTeacherDetails', verifyToken, (req, res) => {
   jwt.verify(req.token, 'secretkey', (err, authData) => {
-    if(err) {
+    if (err) {
       res.sendStatus(403);
     } else {
       let sql = "SELECT * FROM teacher_details";
@@ -111,169 +111,15 @@ app.get('/api/getTeacherDetails', verifyToken, (req, res) => {
 });
 
 
-
 //show single teacher details by user ID
-app.get('/api/getTeacherDetailsByUserID/:userId', (req, res) => {
-  let sql = "SELECT * FROM teacher_details WHERE user_id=" + req.params.userId;
-  let query = conn.query(sql, (err, result) => {
-    if (err) throw err;
-    else {
-      return res.json({
-        data: result
-      })
-    }
-  });
-});
-
-//add new Teacher Details
-app.post('/api/postTeacherDetails', (req, res) => {
-  let teacherDetails = {
-    user_id: req.body.user_id,
-    first_name: req.body.first_name,
-    last_name: req.body.last_name,
-    dob: req.body.dob,
-    email: req.body.email,
-    mobile: req.body.mobile,
-    city: req.body.city
-  };
-
-  let sqlTeacherDetails = "INSERT INTO teacher_details SET ?";
-  let query = conn.query(sqlTeacherDetails, teacherDetails, (err, result) => {
-    if (err) throw err;
-    res.send(JSON.stringify({"status": 200, "error": null, "response": result}));
-    //console.log(result.insertId);
-  });
-});
-
-
-//add new Teacher Qualification
-app.post('/api/postTeacherQualification', (req, res) => {
-  let teacherQualification = {
-    teacher_details_id: req.body.teacher_details_id,
-    experience_year: req.body.experience_year,
-    experience_month: req.body.experience_month
-  };
-
-  let sqlTeacherQualification = "INSERT INTO teacher_qualification SET ?";
-  let query = conn.query(sqlTeacherQualification, teacherQualification, (err, result) => {
-    if (err) throw err;
-    res.send(JSON.stringify({"status": 200, "error": null, "response": result}));
-    //console.log(result.insertId);
-    //Start
-    let qualifications = req.body.qualifications;
-
-    for (let qualification of qualifications) {
-      let teacherQualiSpecialization = {
-        teacher_qualification_id: result.insertId,
-        qulification_id: qualification.qulification_id,
-        specialization_id: qualification.specialization_id,
-        major: qualification.major,
-        minor: qualification.minor,
-        course_completion_date: qualification.course_completion_date
-      }
-
-      let sqlTeacherQualiSpecialization = "INSERT INTO teacher_qualification_specialization SET ?";
-      let query2 = conn.query(sqlTeacherQualiSpecialization, teacherQualiSpecialization, (err, result) => {
+app.get('/api/getTeacherDetailsByUserID/:userId', verifyToken, (req, res) => {
+  jwt.verify(req.token, 'secretkey', (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      let sql = "SELECT * FROM teacher_details WHERE user_id=" + req.params.userId;
+      let query = conn.query(sql, (err, result) => {
         if (err) throw err;
-        //res.send(JSON.stringify({"status": 200, "error": null, "response": result}));
-
-        //let teacherAvailabilityDays = req.body.availibility.availibility_days;
-        let teacherQualiUnits = qualification.units.map(
-          unit => [
-            result.insertId,
-            unit.unit
-          ]);
-
-        let sqlTeacherQualiUnits = "INSERT INTO teacher_qualification_unit (teacher_qualification_specialization_id, unit) VALUES ?";
-        let query3 = conn.query(sqlTeacherQualiUnits, [teacherQualiUnits], (err, result) => {
-          if (err) throw err;
-          //res.send(JSON.stringify({ "status": 200, "error": null, "response": result }));
-        });
-      });
-    }
-    //End
-  });
-});
-
-
-//get all Qualifications
-app.get('/api/getQualifications', (req, res) => {
-  let sql = "SELECT * FROM qualification";
-  let query = conn.query(sql, (err, result) => {
-    if (err) throw err;
-    else {
-      return res.json({
-        data: result
-      })
-    }
-  });
-});
-
-//add new Qualification
-app.post('/api/postQualification',(req, res) => {
-  let data = {
-    name: req.body.name, 
-  };
-
-  let sql = "INSERT INTO qualification SET ?";
-  let query = conn.query(sql, data,(err, results) => {
-    if(err) throw err;
-    res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
-  });
-});
-
-
-//get Qualification By QualificationID
-app.get('/api/getQualificationByID/:qualificationId', (req, res) => {
-  let sql = "SELECT * FROM qualification WHERE id=" + req.params.qualificationId;
-  let query = conn.query(sql, (err, result) => {
-    if (err) throw err;
-    else {
-      return res.json({
-        data: result
-      })
-    }
-  });
-});
-
-//get Specializations By QualificationID
-app.get('/api/getSpecializationsByQualificationID/:qualificationId', (req, res) => {
-  let sql = "SELECT * FROM specialization WHERE qualification_id=" + req.params.qualificationId;
-  let query = conn.query(sql, (err, result) => {
-    if (err) throw err;
-    else {
-      return res.json({
-        data: result
-      })
-    }
-  });
-});
-
-//add new Specialization
-app.post('/api/postSpecialization',(req, res) => {
-  let data = {
-    qualification_id: req.body.qualification_id,
-    name: req.body.name, 
-  };
-
-  let sql = "INSERT INTO  specialization SET ?";
-  let query = conn.query(sql, data,(err, results) => {
-    if(err) throw err;
-    res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
-  });
-});
-
-//get Units By TeacherQualificationId
-app.get('/api/getUnitsBySpecializationID/:teacherQualificationId', (req, res) => {
-  let sqlTeacherQualificationSpecialization = "SELECT id FROM teacher_qualification_specialization WHERE 	teacher_qualification_id=" + req.params.teacherQualificationId;
-  let query = conn.query(sqlTeacherQualificationSpecialization, (err, result) => {
-    if (err) throw err;
-    else {
-      let teacherQualificationSpecializationIds = result.map( function(el) { return el.id; });
-
-      let sqlTeacherQualificationUnit = "SELECT * FROM teacher_qualification_unit WHERE teacher_qualification_specialization_id IN (?)";
-      let query2 = conn.query(sqlTeacherQualificationUnit, [teacherQualificationSpecializationIds], (err, result) => {
-        if (err) throw err; 
         else {
           return res.json({
             data: result
@@ -285,22 +131,259 @@ app.get('/api/getUnitsBySpecializationID/:teacherQualificationId', (req, res) =>
 });
 
 
+//add new Teacher Details
+app.post('/api/postTeacherDetails', verifyToken, (req, res) => {
+  jwt.verify(req.token, 'secretkey', (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      let teacherDetails = {
+        user_id: req.body.user_id,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        dob: req.body.dob,
+        email: req.body.email,
+        mobile: req.body.mobile,
+        city: req.body.city
+      };
 
+      let sqlTeacherDetails = "INSERT INTO teacher_details SET ?";
+      let query = conn.query(sqlTeacherDetails, teacherDetails, (err, result) => {
+        if (err) throw err;
+        res.send(JSON.stringify({ "status": 200, "error": null, "response": result }));
+        //console.log(result.insertId);
+      });
+    }
+  });
+});
+
+//add new Teacher Qualification
+app.post('/api/postTeacherQualification', verifyToken, (req, res) => {
+  jwt.verify(req.token, 'secretkey', (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      let teacherQualification = {
+        teacher_details_id: req.body.teacher_details_id,
+        experience_year: req.body.experience_year,
+        experience_month: req.body.experience_month
+      };
+
+      let sqlTeacherQualification = "INSERT INTO teacher_qualification SET ?";
+      let query = conn.query(sqlTeacherQualification, teacherQualification, (err, result) => {
+        if (err) throw err;
+        res.send(JSON.stringify({ "status": 200, "error": null, "response": result }));
+        //console.log(result.insertId);
+        //Start
+        let qualifications = req.body.qualifications;
+
+        for (let qualification of qualifications) {
+          let teacherQualiSpecialization = {
+            teacher_qualification_id: result.insertId,
+            qulification_id: qualification.qulification_id,
+            specialization_id: qualification.specialization_id,
+            major: qualification.major,
+            minor: qualification.minor,
+            course_completion_date: qualification.course_completion_date
+          }
+
+          let sqlTeacherQualiSpecialization = "INSERT INTO teacher_qualification_specialization SET ?";
+          let query2 = conn.query(sqlTeacherQualiSpecialization, teacherQualiSpecialization, (err, result) => {
+            if (err) throw err;
+            //res.send(JSON.stringify({"status": 200, "error": null, "response": result}));
+
+            //let teacherAvailabilityDays = req.body.availibility.availibility_days;
+            let teacherQualiUnits = qualification.units.map(
+              unit => [
+                result.insertId,
+                unit.unit
+              ]);
+
+            let sqlTeacherQualiUnits = "INSERT INTO teacher_qualification_unit (teacher_qualification_specialization_id, unit) VALUES ?";
+            let query3 = conn.query(sqlTeacherQualiUnits, [teacherQualiUnits], (err, result) => {
+              if (err) throw err;
+              //res.send(JSON.stringify({ "status": 200, "error": null, "response": result }));
+            });
+          });
+        }
+        //End
+      });
+    }
+  });
+});
+
+//get all Qualifications
+app.get('/api/getQualifications', verifyToken, (req, res) => {
+  jwt.verify(req.token, 'secretkey', (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      let sql = "SELECT * FROM qualification";
+      let query = conn.query(sql, (err, result) => {
+        if (err) throw err;
+        else {
+          return res.json({
+            data: result
+          })
+        }
+      });
+    }
+  });
+});
+
+//add new Qualification
+app.post('/api/postQualification', verifyToken, (req, res) => {
+  jwt.verify(req.token, 'secretkey', (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      let data = {
+        name: req.body.name,
+      };
+
+      let sql = "INSERT INTO qualification SET ?";
+      let query = conn.query(sql, data, (err, results) => {
+        if (err) throw err;
+        res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
+      });
+    }
+  });
+});
+
+//get Qualification By QualificationID
+app.get('/api/getQualificationByID/:qualificationId', verifyToken, (req, res) => {
+  jwt.verify(req.token, 'secretkey', (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      let sql = "SELECT * FROM qualification WHERE id=" + req.params.qualificationId;
+      let query = conn.query(sql, (err, result) => {
+        if (err) throw err;
+        else {
+          return res.json({
+            data: result
+          })
+        }
+      });
+    }
+  });
+});
+
+//get Specializations By QualificationID
+app.get('/api/getSpecializationsByQualificationID/:qualificationId', verifyToken, (req, res) => {
+  jwt.verify(req.token, 'secretkey', (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      let sql = "SELECT * FROM specialization WHERE qualification_id=" + req.params.qualificationId;
+      let query = conn.query(sql, (err, result) => {
+        if (err) throw err;
+        else {
+          return res.json({
+            data: result
+          })
+        }
+      });
+    }
+  });
+});
+
+//add new Specialization
+app.post('/api/postSpecialization', verifyToken, (req, res) => {
+  jwt.verify(req.token, 'secretkey', (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      let data = {
+        qualification_id: req.body.qualification_id,
+        name: req.body.name,
+      };
+
+      let sql = "INSERT INTO  specialization SET ?";
+      let query = conn.query(sql, data, (err, results) => {
+        if (err) throw err;
+        res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
+      });
+    }
+  });
+});
+
+//get Units By TeacherQualificationId
+app.get('/api/getUnitsBySpecializationID/:teacherQualificationId', verifyToken, (req, res) => {
+  jwt.verify(req.token, 'secretkey', (err, authData) => {
+    if (err) {
+      res.sendStatus(403);
+    } else {
+      let sqlTeacherQualificationSpecialization = "SELECT id FROM teacher_qualification_specialization WHERE 	teacher_qualification_id=" + req.params.teacherQualificationId;
+      let query = conn.query(sqlTeacherQualificationSpecialization, (err, result) => {
+        if (err) throw err;
+        else {
+          let teacherQualificationSpecializationIds = result.map(function (el) { return el.id; });
+
+          let sqlTeacherQualificationUnit = "SELECT * FROM teacher_qualification_unit WHERE teacher_qualification_specialization_id IN (?)";
+          let query2 = conn.query(sqlTeacherQualificationUnit, [teacherQualificationSpecializationIds], (err, result) => {
+            if (err) throw err;
+            else {
+              return res.json({
+                data: result
+              })
+            }
+          });
+        }
+      });
+    }
+  });
+});
+
+//Registrater new user
+app.post('/api/registration', (req, res) => {
+  let user = {
+    email: req.body.email,
+    password: req.body.password
+  };
+
+  let sqlFindUser = "SELECT * FROM user WHERE email='" + user.email + "' AND password='" + user.password + "'";
+  let query = conn.query(sqlFindUser, (err, results) => {
+    if (err) throw err;
+    else {
+      if (results.length != 1) {
+        let sqlInserUser = "INSERT INTO user SET ?";
+        let query = conn.query(sqlInserUser, user, (err, results) => {
+          if (err) throw err;
+          res.send(JSON.stringify({ "status": 200, "error": null, "response": results }));
+        });
+      }
+      else {
+        res.send(JSON.stringify({ "status": 200, "error": "user already exist", "response": results }));
+      }
+    }
+  });
+});
 
 
 
 app.post('/api/login', (req, res) => {
-  // Mock user
-  const user = {
-    id: 1, 
-    username: 'Asif',
-    email: 'asif@gmail.com'
-  }
+  //Mock user
+  let user = {
+    email: req.body.email,
+    password: req.body.password
+  };
 
-  jwt.sign({user}, 'secretkey', { expiresIn: '1h' }, (err, token) => {
-    res.json({
-      token
-    });
+  let sqlFindUser = "SELECT * FROM user WHERE email='" + user.email + "' AND password='" + user.password + "'";
+  let query = conn.query(sqlFindUser, (err, results) => {
+    if (err) throw err;
+    else {
+      if (results.length == 1) {
+        jwt.sign({ user }, 'secretkey', { expiresIn: '1h' }, (err, token) => {
+          res.json({
+            token
+          });
+        });
+      }
+      else {
+        res.send(JSON.stringify({ "status": 200, "error": "user deesn't exist", "response": results }));
+      }
+    }
   });
 });
 
@@ -313,7 +396,7 @@ function verifyToken(req, res, next) {
   // Get auth header value
   const bearerHeader = req.headers['authorization'];
   // Check if bearer is undefined
-  if(typeof bearerHeader !== 'undefined') {
+  if (typeof bearerHeader !== 'undefined') {
     // Split at the space
     const bearer = bearerHeader.split(' ');
     // Get token from array
